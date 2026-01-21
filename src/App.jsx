4f785882d221cfd5d7b81e2936a5808719a6b53d
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2026-01-20 19:24:26 -0700

    use /hikes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
   const [data, setData] = useState()
 
   async function fetchData() {
-    const response = await fetch('https://hiking-backend.vercel.app');
+    const response = await fetch('https://hiking-backend.vercel.app/hikes');
+    // const response = await fetch('http://localhost:3000/hikes');
     const tempData = await response.json();
     setData(tempData);
   }
