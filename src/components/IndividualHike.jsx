3c820dc69f6eb35c6e692439691df584db8ebb65
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2026-02-21 12:42:55 -0700

    display map of path on individual hike

--- src/components/IndividualHike.jsx
+++ src/components/IndividualHike.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
+import { parseCoordsArray } from "../utils";
+import DisplayPath from "./DisplayPath";
 
 export default function IndividualHike() {
   const { id } = useParams();
@@ -23,7 +25,7 @@ export default function IndividualHike() {
         <>
           <h3>ID:&nbsp;{hike.id}</h3>
           <p>Name:&nbsp;{hike.name}</p>
-          <p>Location:&nbsp;{hike.location}</p>
+          <DisplayPath pathData={parseCoordsArray(hike.location)} />
           <p>Description:&nbsp;{hike.description}</p>
           <p>Created At:&nbsp;{hike.createdAt}</p>
           <p>Updated At:&nbsp;{hike.updatedAt}</p>
